4788f1d56c17728a3ed9f7eae2f1e8b2dcff1cb8
DavidParedesRodriguez <deividregalon2002@gmail.com>
2023-12-19 09:46:10 +0100

    instalar_y_configurar_Angular

---
+++ 01-Bases/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({ //esto es lo que se llama decorador
+  selector: 'app-root',//será la aplicación principal. Si nos vamos al index.html podemos ver el componente, pasando antes por otros procesos.
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+ // title = 'bases'; //nombre del proyecto
+
+ //cambia en la clase AppComponent el nombre del título a "contador App"
+ titulo : string = 'contador app'
+}
