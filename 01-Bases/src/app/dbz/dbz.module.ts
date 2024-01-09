f04ef1676cfad1e5677d51ab5149b9d4afd3ea75
DavidParedesRodriguez <deividregalon2002@gmail.com>
2024-01-09 12:24:37 +0100

    listarPersonajes_hecho_mi_forma

--- 01-Bases/src/app/dbz/dbz.module.ts
+++ 01-Bases/src/app/dbz/dbz.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule, FormsModule
   ]
 })
 export class DbzModule { }
--- 01-Bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-Bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
+
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
 
  public titulo : String = "Dragon Ball Z";
+
+ //Ejercicio #11 Diseño página inicial 
+ public personajes : String = "Personajes";
+ public tituloPersonaje : String = "Agregar nuevo Personajes";
+
+ nuevoPersonaje: any = {};
+  listaPersonajes: any[] = [];
+
+  agregarPersonaje() {
+    if (this.nuevoPersonaje.nombre && this.nuevoPersonaje.poder) {
+      this.listaPersonajes.push({ ...this.nuevoPersonaje });
+      // Limpiar los campos después de agregar el personaje
+      this.nuevoPersonaje = { nombre: '', poder: '' };
+    } else {
+      alert('Por favor, complete todos los campos.');
+    }
+  }
 }
