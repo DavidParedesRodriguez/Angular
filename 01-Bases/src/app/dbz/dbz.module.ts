fadcce6143fbd3c800533d1466936142ce35ebb2
DavidParedesRodriguez <deividregalon2002@gmail.com>
2024-01-22 13:36:00 +0100

    dbz_hecho

--- 01-Bases/src/app/dbz/dbz.module.ts
+++ 01-Bases/src/app/dbz/dbz.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule, FormsModule
   ]
 })
 export class DbzModule { }
--- 01-Bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-Bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,35 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Personaje } from '../interfaces/dbz.interfaces';
-
-interface Personaje {
-  //crreamos una interface
-  nombre: string;
-  poder: number;
-}
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
 
 @Component({
   selector: 'app-main-page',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './main-page.component.html',
- 
+  styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
 
-  
-personaje : Personaje[] = [
-  {
-    nombre: 'Goku',
-    poder: 15000
-  },
-  {
-    nombre: 'Vegeta',
-    poder: 7500
+ public titulo : String = "Dragon Ball Z";
+
+ //Ejercicio #11 Diseño página inicial 
+ public personajes : String = "Personajes";
+ public tituloPersonaje : String = "Agregar nuevo Personajes";
+
+ nuevoPersonaje: any = {};
+  listaPersonajes: any[] = [];
+
+  agregarPersonaje() {
+    if (this.nuevoPersonaje.nombre && this.nuevoPersonaje.poder) {
+      this.listaPersonajes.push({ ...this.nuevoPersonaje });
+      // Limpiar los campos después de agregar el personaje
+      this.nuevoPersonaje = { nombre: '', poder: '' };
+    } else {
+      alert('Por favor, complete todos los campos.');
+    }
   }
-];
-
-
-nuevo: Personaje = {
-  nombre: "Genio Tortuga",
-  poder: 1000
 }
 
-}
