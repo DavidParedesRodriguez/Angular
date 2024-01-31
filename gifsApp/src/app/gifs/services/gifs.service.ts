12968400513468c4d6de358b6faae7c8fb53878b
DavidParedesRodriguez <deividregalon2002@gmail.com>
2024-01-31 08:59:00 +0100

    gifsApp_hecho

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,41 +1,71 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { SearchGIFResponse } from '../interface/gifs.interface';
+import { Gif, SearchGIFResponse } from '../interface/gifs.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
-
-  private apiKey: string = "yjGnI6lLOf3jlz2EP6aiMulk5q4r3A2I";
-  private _historial: string[] = []
-
-
-
+  private apiKey: string = 'yjGnI6lLOf3jlz2EP6aiMulk5q4r3A2I';
+  private servicioUrl : string = 'https://api.giphy.com/v1/gifs';
+  private _historial: string[] = [];
   public resultados: Gif[] = [];
-  get historial(){
-    return[...this._historial];
+
+  get historial() {
+    return [...this._historial];
   }
 
-  constructor(private http: HttpClient){} //Con este http ya puedo hacer peticiones
+  constructor(private http: HttpClient) {
+    if (
+      typeof localStorage !== 'undefined' &&
+      localStorage.getItem('historial')
+    ) {
+      this._historial = JSON.parse(localStorage.getItem('historial')!);
+    }
 
-  buscarGifs(query:string){
+    // Cargar resultados desde localStorage al iniciar el servicio
+    if (
+      typeof localStorage !== 'undefined' &&
+      localStorage.getItem('resultados')
+    ) {
+      this.resultados = JSON.parse(localStorage.getItem('resultados')!);
+    }
+  }
+
+  buscarGifs(query: string) {
     // Verificar si el query ya está en el historial
     if (!this._historial.includes(query)) {
       // Si no está, agregarlo al principio del historial
       this._historial.unshift(query);
       // Limitar el historial a los últimos 10 elementos
-      this._historial = this._historial.slice(0, 10);
+      this._historial = this._historial.splice(0, 10);
       //console.log(this._historial);
+
+      if (typeof localStorage !== 'undefined') {
+        localStorage.setItem('historial', JSON.stringify(this._historial));
+      }
     }
 
-    this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=yjGnI6lLOf3jlz2EP6aiMulk5q4r3A2I&q=${ query }&limit=10`)
-    .subscribe((resp) => {
-      console.log(resp.data);
-      this.resultados = resp.data;
-    })
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
 
-/*
+    console.log(params.toString());
+
+    //yjGnI6lLOf3jlz2EP6aiMulk5q4r3A2I
+    this.http.get<SearchGIFResponse>(`${this.servicioUrl}/search`,{ params})
+      .subscribe((resp) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+
+        // Guardar resultados en localStorage
+        if (typeof localStorage !== 'undefined') {
+          localStorage.setItem('resultados', JSON.stringify(this.resultados));
+        }
+      });
+
+    /*
     fetch("https://api.giphy.com/v1/gifs/search?api_key=yjGnI6lLOf3jlz2EP6aiMulk5q4r3A2I&q=dragon ball z&limit=10")
     .then(resp =>{
       resp.json().then(data => {
--- gifsApp/src/app/sidebar/sidebar.component.ts
+++ gifsApp/src/app/sidebar/sidebar.component.ts
@@ -8,8 +8,26 @@ import { GifsService } from '../gifs/services/gifs.service';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent implements OnInit{
-  
+
 constructor(private GifsService: GifsService){}
+
+
+buscar(termino: string) {
+  console.log(termino);
+
+  // Llamar al método buscarGifs del servicio para cargar las imágenes
+  this.GifsService.buscarGifs(termino);
+
+  // Aplicar la clase de animación al componente de imágenes (opcional)
+  this.aplicarAnimacion();
+}
+
+
+aplicarAnimacion() {
+  // Agrega la clase de animación al componente de imágenes
+  const imagenesComponent = document.getElementById('imagenes-component');
+  imagenesComponent?.classList.add('animate__animated', 'animate__fadeIn');
+}
 
 ngOnInit(): void {}
 
